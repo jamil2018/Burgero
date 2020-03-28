879cbda50c84dd60c35936104d0730dd13c4a7d9
Hasnat Jamil <hasnatjamil2018@gmail.com>
2020-03-28 17:44:47 +0600

    Initial commit: 1. Create project using created react app 2. Clean up initial react app code 3. Add font reference 4. Set up project to use css modules.

---
+++ burgero.web/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import style from "./app.module.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className={style.h1}>Hello World</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium ipsa
+        tempore deleniti at quis ducimus similique delectus soluta expedita?
+        Inventore accusantium eos dolorem voluptatem quae.
+      </p>
+    </div>
+  );
+}
+
+export default App;
